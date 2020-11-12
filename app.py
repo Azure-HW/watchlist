574d8bdff304d3f1627219f1fff0c6a4b9469105
Azure-HW <841308027@qq.com>
2020-11-12 16:52:02 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import url_for
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    return render_template('index.html',name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -21,3 +22,17 @@ def test_url_for():
     print(url_for('test_url_for', num=2))
 
     return 'Test Page'
+
+name = "Hewei"
+movies=[
+    {'title': 'My Neighbor Totoro', 'year': '1998'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
